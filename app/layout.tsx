79a010091f9eb549d3889564d9c9d39c01f31211
OCbwoy3 <95974645+ocbwoy3@users.noreply.github.com>
2024-10-10 20:35:16 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 				<Suspense>
 					{/*<div className="absolute bottom-[10px] left-[10px] text-xs text-muted-foreground z-[9999]">
 						© OCbwoy3 2024-present &mdash; <a href="https://github.com/ocbwoy3/ocbwoy3.dev/" className="text-blue-500 underline pointer-events-auto z-501">Source</a><br/>
-						APIs for Karma, Nova and the Goober Project are provided by third parties.*/}
+						APIs for Karma and the Goober Project are provided by third parties.*/}
 						{/* &mdash; <a href="/legal" className="text-blue-500 underline pointer-events-auto z-501">Terms</a> */}
 					</div>
 				</Suspense>
